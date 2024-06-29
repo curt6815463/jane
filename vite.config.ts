a589de6bbb870a94474b71b9333b189b89fcf60c
curt <curt6815463@gmail.com>
2024-06-29 21:40:31 +0800

    add deploy yam

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/jane/',
   resolve: {
     alias: {
       '@mui/styled-engine': '@mui/styled-engine-sc'
