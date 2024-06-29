b7a28c35ec869a379efb06b59f767c72e86e2584
curt <curt6815463@gmail.com>
2024-06-29 19:12:25 +0800

    adjust intro video

--- src/Intro.tsx
+++ src/Intro.tsx
@@ -1,28 +1,90 @@
+import styled from "styled-components";
+import audioSrc from "./assets/testaudio.m4a";
+import { Button } from "@mui/material";
+import { useNavigate } from "react-router-dom";
+const P1 = styled.div`
+  font-size: 28px;
+  padding: 20px;
+`;
+
+const P = styled.div`
+  font-size: 20px;
+  padding: 20px;
+  line-height: 28px;
+`;
+
+const P3 = styled(P)`
+  text-indent: 40px;
+`;
+
+const ButtonWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+`;
+
 const Intro = () => {
+  const navigate = useNavigate();
+
   return (
-    <div style={{ fontSize: "18px" }}>
-      <div>Dear Jane:</div>
-      <div>在知道結果之前，有些話想跟妳說</div>
-      <div>
-        今天是我們相識的第{"120"}天，從這段時間的相處 覺得妳是很酷的人，
-        因為在一些事情上我以為女生會這樣想，但妳卻不是
-        這對我來說很讚，之前可能把女生想得太壞ＸＤ（也可能認識的女生太少）
-        雖然妳常說求包養，但我知道妳只是想找個兼職居家清潔（？！
-      </div>
-      <div>
-        從過去的聊天中，妳也會講訴一些對事情的看法，讓我知道妳是個既聰明又有想法的人，
-        不是天然呆少女，所有事情都只讓別人幫她完成
-        即使眼睛不好，也會找尋其他方式來增加自己的知識、見識，
+    <div style={{ fontSize: "18px", paddingBottom: "30px" }}>
+      <P1>
+        Dear <span style={{ color: "#f199a9" }}>Jane</span>:
+      </P1>
+      <audio controls>
+        <source src={audioSrc} type="audio/mp4" />
+      </audio>
+      <P3>
+        在知道結果之前，有些話想說，今天是我們相識的第73天，從這段時間的相處，覺得妳是很酷的人，
+        因為在一些事情上有時我以為女生都是這麼想，但妳卻沒有，
+        這對我來說很棒，因為之前可能把女生想得很壞ＸＤ，但也可能是我認識的女生太少。
+      </P3>
+      <P3>
+        妳的生活樸實，平凡中而有些變化，偶爾規劃出門旅行，平常時候認真上班，
+        不喜歡浪費東西，也不浪費錢，東西還能用就用，
+        我覺得生活中的步調就是有空的跟滿的，才能細細品味其中的差異，
+        這也是我喜歡的生活模式，希望未來有機會能與妳一起維持這和諧節奏。
+      </P3>
+      <P3>
+        從過去的聊天中，妳也會講訴一些對事情的看法，讓我知道妳是個既聰明又有想法的人，即使眼睛不好，也會找尋其他方式來增加自己的知識、見識，
+        妳不是天然呆少女，妳會規劃好自己想做的事，每天穩穩地走著屬於妳的人生旅程，
         在人生的路上雖然遇到一些顛頗，但還是能夠含著淚往前，
-        即使跌落到谷底可能需要別人伸出援手時，你還是靠著自己努力爬上懸崖，
-        妳很堅強很優秀！
-      </div>
-      <div>
-        妳的生活樸實，平凡中有些變化，偶爾規劃出門旅行，平常認真上班
-        不喜歡浪費東西，東西還能使用就用，也不是屬於浪費錢的人
-        我覺得生活中的步調就是有空的跟滿的，才能細細品味其中的差異
-        這也是我喜歡的生活模式
-      </div>
+        即使跌落到谷底需要別人伸出援手時，妳還是靠著自己努力爬上懸崖，
+        妳很堅強很優秀！我想妳自己也知道！
+        希望在妳的未來，能夠有另外一位人生旅伴，在妳難過的時候陪妳哭，在妳快樂的時候陪妳笑，在妳有困難的時候陪妳一起攜手共渡難關
+      </P3>
+      <ButtonWrapper>
+        <Button
+          sx={{
+            marginRight: "16px",
+            fontSize: "20px",
+            boxShadow: "none",
+            marginTop: "20px",
+            backgroundColor: "#f199a9",
+            "&:hover": { backgroundColor: "#f199a9" },
+          }}
+          variant="contained"
+          onClick={() => {
+            navigate(-1);
+          }}
+        >
+          BACK
+        </Button>
+        <Button
+          sx={{
+            fontSize: "20px",
+            boxShadow: "none",
+            marginTop: "20px",
+            backgroundColor: "#f199a9",
+            "&:hover": { backgroundColor: "#f199a9" },
+          }}
+          variant="contained"
+          onClick={() => {
+            navigate("/video");
+          }}
+        >
+          Next
+        </Button>
+      </ButtonWrapper>
     </div>
   );
 };
@@ -32,4 +94,7 @@ const Intro = () => {
 // 節省
 // 自我管理
 //
+// a juncture
+
+// jane curt
 export default Intro;
--- src/VideoShow.tsx
+++ src/VideoShow.tsx
@@ -1,16 +1,58 @@
+import { Button } from "@mui/material";
+import { useNavigate } from "react-router-dom";
+import styled from "styled-components";
+
+const VideoShowStyled = styled.div``;
+const ButtonWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+`;
 const VideoShow = () => {
+  const height = window.innerHeight - 70 - 100;
+  const navigate = useNavigate();
+
   return (
-    <div>
+    <VideoShowStyled>
       <iframe
         width="100%"
-        height="100%"
-        src="https://www.youtube.com/embed/-MkClg7XgRI"
-        title="Does YouTube Want More Vertical Videos?"
+        height={height}
+        src="https://www.youtube.com/embed/p0Cb4TQG28g"
+        title="Jane"
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
         referrerPolicy="strict-origin-when-cross-origin"
-        allowFullScreen
       ></iframe>
-    </div>
+      <ButtonWrapper>
+        <Button
+          sx={{
+            marginRight: "16px",
+            fontSize: "20px",
+            boxShadow: "none",
+            marginTop: "20px",
+            backgroundColor: "#f199a9",
+            "&:hover": { backgroundColor: "#f199a9" },
+          }}
+          variant="contained"
+          onClick={() => {
+            navigate(-1);
+          }}
+        >
+          BACK
+        </Button>
+        <Button
+          sx={{
+            fontSize: "20px",
+            boxShadow: "none",
+            marginTop: "20px",
+            backgroundColor: "#f199a9",
+            "&:hover": { backgroundColor: "#f199a9" },
+          }}
+          variant="contained"
+          onClick={() => {}}
+        >
+          Next
+        </Button>
+      </ButtonWrapper>
+    </VideoShowStyled>
   );
 };
 
